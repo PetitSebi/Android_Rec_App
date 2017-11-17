5caf2505b9258ba65a5e6371ac74a9c585ee0278
valentin-braescu <valentin.braescukerlan@gmail.com>
2017-11-17 14:00:07 +0100

    Added Toolbar + Start Recycler View

--- project/app/build/generated/source/r/debug/android/support/coreutils/R.java
+++ project/app/build/generated/source/r/debug/android/support/coreutils/R.java
@@ -94,12 +94,12 @@ public final class R {
         public static final int status_bar_notification_info_maxnum = 0x7f0c0004;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f040020;
-        public static final int notification_action_tombstone = 0x7f040021;
-        public static final int notification_template_custom_big = 0x7f040028;
-        public static final int notification_template_icon_group = 0x7f040029;
-        public static final int notification_template_part_chronometer = 0x7f04002d;
-        public static final int notification_template_part_time = 0x7f04002e;
+        public static final int notification_action = 0x7f040021;
+        public static final int notification_action_tombstone = 0x7f040022;
+        public static final int notification_template_custom_big = 0x7f040029;
+        public static final int notification_template_icon_group = 0x7f04002a;
+        public static final int notification_template_part_chronometer = 0x7f04002e;
+        public static final int notification_template_part_time = 0x7f04002f;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f060014;
